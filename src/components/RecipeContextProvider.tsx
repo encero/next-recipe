88b9faf9a4ff590fc6b524b5cc8a29b836ee4a1b
Matous Michalik <i@matousmichalik.com>
2025-08-14 16:58:01 +0200

    catch covex error on recipe list load

--- src/components/RecipeContextProvider.tsx
+++ src/components/RecipeContextProvider.tsx
@@ -1,15 +1,26 @@
 "use client"
 
 import { useQuery } from "convex/react";
+import { redirect } from "next/navigation";
+import router from "next/router";
 import { createContext, useContext } from "react";
 import { api } from "~/convex/_generated/api";
+import { tryCatch, tryCatchSync } from "~/lib/utils";
 import type { Recipe } from "~/types/recipe";
 
 const RecipeListContext = createContext<Recipe[] | undefined>(undefined);
 
 export function RecipeContextProvider({ children }: { children: React.ReactNode }) {
 
-    const recipes = useQuery(api.recipes.listRecipes);
+    const { data: recipes, error } = tryCatchSync(() => useQuery(api.recipes.listRecipes));
+
+    if (error) {
+        if (error instanceof Error && error.message.includes("Not authenticated")) {
+            redirect("/");
+        }
+        console.error(error);
+        throw error;
+    }
 
     return (
         <RecipeListContext.Provider value={recipes}>
